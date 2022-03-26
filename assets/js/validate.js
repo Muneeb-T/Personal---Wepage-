29eed4e7488ee90d7dbe69ee509a413dec0a0cab
Muneeb-T <88328343+Muneeb-T@users.noreply.github.com>
2022-03-26 16:13:58 +0530

    Update validate.js

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,40 +1,31 @@
-
-$(document).ready(function () {
-    $("#contact-form").validate({
-        errorClass: "validate",
-        rules: {
-            // simple rule, converted to {required:true}
-            name: "required",
-            // compound rule
-            email: {
-                required: true,
-                email: true
-            },
-            subject: "required",
-            message: {
-                required : true,
-                minlength: 50,
-            }
+$("#gform").validate({
+    errorClass: "validate",
+    rules: {
+        name: "required",
+        email: {
+            required: true,
+            email: true
         },
-        messages:{
-            name:{
-                required : "Please enter name"
-            },
-            email:{
-                required: "Please enter email address",
-                email:"Please enter valid email"
-            },
-            subject:{
-                required:"Please enter subject"
-            },
-            message:{
-                required:"Please type your message here",
-                minlength:"Message must be in miniumum 50 characters"
-            }
+        subject: "required",
+        message: {
+            required: true,
+            minlength: 50,
         }
-    });
+    },
+    messages: {
+        name: {
+            required: "Please enter name"
+        },
+        email: {
+            required: "Please enter email address",
+            email: "Please enter valid email"
+        },
+        subject: {
+            required: "Please enter subject"
+        },
+        message: {
+            required: "Please type your message here",
+            minlength: "Message must be in miniumum 50 characters"
+        }
+    }
 });
-
-
-
-
